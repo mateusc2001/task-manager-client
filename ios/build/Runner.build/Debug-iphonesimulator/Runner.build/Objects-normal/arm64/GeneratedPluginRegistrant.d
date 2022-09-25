d90c04d095e368ddf42ac87bad88f36b19b01cbb
Mateus Camargo Rodrigues <mateus-mac@Mateuss-MacBook-Pro.local>
2022-09-25 02:04:47 -0300

    Initial config and login screen

---
+++ ios/build/Runner.build/Debug-iphonesimulator/Runner.build/Objects-normal/arm64/GeneratedPluginRegistrant.d
@@ -0,0 +1,20 @@
+dependencies: \
+  /Users/mateus-mac/StudioProjects/task_manager/ios/Runner/GeneratedPluginRegistrant.m \
+  /Users/mateus-mac/StudioProjects/task_manager/ios/Runner/GeneratedPluginRegistrant.h \
+  /Users/mateus-mac/StudioProjects/task_manager/ios/build/Debug-iphonesimulator/Flutter.framework/Modules/module.modulemap \
+  /Users/mateus-mac/StudioProjects/task_manager/ios/build/Debug-iphonesimulator/Flutter.framework/Headers/FlutterViewController.h \
+  /Users/mateus-mac/StudioProjects/task_manager/ios/build/Debug-iphonesimulator/Flutter.framework/Headers/FlutterPluginAppLifeCycleDelegate.h \
+  /Users/mateus-mac/StudioProjects/task_manager/ios/build/Debug-iphonesimulator/Flutter.framework/Headers/FlutterHeadlessDartRunner.h \
+  /Users/mateus-mac/StudioProjects/task_manager/ios/build/Debug-iphonesimulator/Flutter.framework/Headers/FlutterEngineGroup.h \
+  /Users/mateus-mac/StudioProjects/task_manager/ios/build/Debug-iphonesimulator/Flutter.framework/Headers/FlutterEngine.h \
+  /Users/mateus-mac/StudioProjects/task_manager/ios/build/Debug-iphonesimulator/Flutter.framework/Headers/FlutterDartProject.h \
+  /Users/mateus-mac/StudioProjects/task_manager/ios/build/Debug-iphonesimulator/Flutter.framework/Headers/FlutterCallbackCache.h \
+  /Users/mateus-mac/StudioProjects/task_manager/ios/build/Debug-iphonesimulator/Flutter.framework/Headers/FlutterTexture.h \
+  /Users/mateus-mac/StudioProjects/task_manager/ios/build/Debug-iphonesimulator/Flutter.framework/Headers/FlutterPlatformViews.h \
+  /Users/mateus-mac/StudioProjects/task_manager/ios/build/Debug-iphonesimulator/Flutter.framework/Headers/FlutterCodecs.h \
+  /Users/mateus-mac/StudioProjects/task_manager/ios/build/Debug-iphonesimulator/Flutter.framework/Headers/FlutterChannels.h \
+  /Users/mateus-mac/StudioProjects/task_manager/ios/build/Debug-iphonesimulator/Flutter.framework/Headers/FlutterBinaryMessenger.h \
+  /Users/mateus-mac/StudioProjects/task_manager/ios/build/Debug-iphonesimulator/Flutter.framework/Headers/FlutterPlugin.h \
+  /Users/mateus-mac/StudioProjects/task_manager/ios/build/Debug-iphonesimulator/Flutter.framework/Headers/FlutterMacros.h \
+  /Users/mateus-mac/StudioProjects/task_manager/ios/build/Debug-iphonesimulator/Flutter.framework/Headers/FlutterAppDelegate.h \
+  /Users/mateus-mac/StudioProjects/task_manager/ios/build/Debug-iphonesimulator/Flutter.framework/Headers/Flutter.h
---
+++ ios/build/Runner.build/Debug-iphonesimulator/Runner.build/Objects-normal/arm64/Runner_vers.d
@@ -0,0 +1,2 @@
+dependencies: \
+  /Users/mateus-mac/StudioProjects/task_manager/ios/build/Runner.build/Debug-iphonesimulator/Runner.build/DerivedSources/Runner_vers.c
